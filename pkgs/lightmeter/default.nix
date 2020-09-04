7ddaf8ec07f5010ed40475bba45ace5d32e21e2b
Edmund Wu <fangkazuto@gmail.com>
2020-09-04 18:12:16 -0400

    lightmeter: fix ldflags without release

--- pkgs/lightmeter/default.nix
+++ pkgs/lightmeter/default.nix
@@ -20,7 +20,7 @@ buildGoModule {
     in [
       # Broken, staticdata.HttpAssets doesn't generate
       # "-tags='release'" # release build includes the website
-      "-ldflags=-X main.Commit=${GIT_COMMIT} -X main.TagOrBranch=${GIT_BRANCH} -X main.Version=${APP_VERSION}"
+      ''-ldflags="-X main.Commit=${GIT_COMMIT} -X main.TagOrBranch=${GIT_BRANCH} -X main.Version=${APP_VERSION}"''
     ];
 
   vendorSha256 = "0lkg20lxrklpn8h2vi8p4zy78fwp1qmiwmqwqpwvpajyrxqmfr7i";
